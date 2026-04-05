2b810665119e3ad6ad098afbcbe87fb7fbf5f2a7
AlexJRQ32 <robleroalex015@icloud.com>
2026-04-04 20:58:15 -0600

    New Files

--- Diseño de Sistemas/Proyecto Programado/Systems_Design_Project_Demo/Systems_Design_Project_Demo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Systems_Design_Project_Demo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
